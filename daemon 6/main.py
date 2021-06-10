7fccf036d06f630ac6536e63ad4dee84f88a30a0
Vitaly <vitalyryabchich@gmail.com>
2021-06-10 14:48:14 +0300

    fixed demon

--- daemon 6/main.py
+++ daemon 6/main.py
@@ -1,38 +1,18 @@
-import sys
-
+import ctypes
 from infi.systray import SysTrayIcon
-import ctypes
-from ctypes import wintypes
-import time
 
 
-
-# def turnOffKeyboard(systray):
-#
-#
-#
-# def turnOnKeyboard(systray):
-# 	print('value')
+def turnOffKeyboard(systray):
+    ctypes.windll.user32.BlockInput(True)
 
 
+def turnOnKeyboard(systray):
+    ctypes.windll.user32.BlockInput(False)
 
 
-# menu_options = (("turn Off Keyboard", None, turnOffKeyboard), ("turn on Keyboard", None, turnOnKeyboard))
-# systray = SysTrayIcon("E://image.ico", "Example tray icon", menu_options)
-# systray.start()
-
-import ctypes
-from ctypes import wintypes
-
-BlockInput = ctypes.windll.user32.BlockInput
-BlockInput.argtypes = [wintypes.BOOL]
-BlockInput.restype = wintypes.BOOL
-
-blocked = BlockInput(True)
-if blocked:
-    try:
-        pass # do something
-    finally:
-        unblocked = BlockInput(False) # unblock in any case
-else:
-    raise RuntimeError('Input is already blocked by another thread!')
+menu_options = (
+    ("turn Off Keyboard", None, turnOffKeyboard),
+    ("turn on Keyboard", None, turnOnKeyboard),
+)
+systray = SysTrayIcon("E://image.ico", "Example tray icon", menu_options)
+systray.start()
